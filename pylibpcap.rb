8806ee09b7f9bc77aced450e109b4f876933eedf
Tim Carmean <chong@bigmaninjapan.com>
2013-09-05 14:51:43 -0400

    fixed sha1

--- pylibpcap.rb
+++ pylibpcap.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Pylibpcap <Formula
 	url 'http://downloads.sourceforge.net/project/pylibpcap/pylibpcap/0.6.4/pylibpcap-0.6.4.tar.gz'
 	homepage 'http://pylibpcap.sourceforge.net/'
-	sha1 'http://pylibpcap.sourceforge.net/'
+	sha1 '30b5d2e9dc8dcf6df067a216d9ecae109ff34f86'
 
 	def install
 		system "python setup.py install"
