c24a5ad73b207f3cc79b16ac084518052df5575f
Tim Carmean <chong@bigmaninjapan.com>
2013-09-05 14:58:29 -0400

    added python dep

--- pylibpcap.rb
+++ pylibpcap.rb
@@ -4,6 +4,8 @@ class Pylibpcap <Formula
 	url 'http://downloads.sourceforge.net/project/pylibpcap/pylibpcap/0.6.4/pylibpcap-0.6.4.tar.gz'
 	homepage 'http://pylibpcap.sourceforge.net/'
 	sha1 '30b5d2e9dc8dcf6df067a216d9ecae109ff34f86'
+	
+	depends_on :python
 
 	def install
 		python do
